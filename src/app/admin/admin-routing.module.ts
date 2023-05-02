5bef50b15f49c27ccdd64cbf9c220ee20735f02a
evertonrivas <evertonrivas@gmail.com>
2023-05-02 16:05:31 -0300

    Novo módulo users

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminComponent } from './admin.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { UsersComponent } from './users/users.component';
 
 const routes: Routes = [{ 
   path: '', 
@@ -9,7 +10,11 @@ const routes: Routes = [{
   children:[{
     path: 'dashboard',
     component: DashboardComponent
-  }] }];
+  },{
+    path: 'users',
+    component: UsersComponent
+  }] 
+}];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
