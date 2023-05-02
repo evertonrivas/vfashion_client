8a6c29631d1b73cf11a95191d3a1a2351acd5485
evertonrivas <evertonrivas@gmail.com>
2023-05-02 16:05:58 -0300

    Versão inicial do dashboard

--- src/app/admin/dashboard/dashboard.component.ts
+++ src/app/admin/dashboard/dashboard.component.ts
@@ -1,10 +1,59 @@
-import { Component } from '@angular/core';
+import { formatCurrency } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { EChartsOption} from 'echarts';
+
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent {
+export class DashboardComponent  implements OnInit{
+  totalValueOrder:number = 0;
+  totalOrder:number = 0;
+  totalRepresentative:number = 0;
+  totalCustomer:number = 0;
+  options:EChartsOption = {
+    tooltip:{
+      trigger: 'axis',
+      axisPointer:{
+        type: 'shadow'
+      },
+      valueFormatter: (value) => formatCurrency(parseFloat(value.toString()),"pt","R$")
+    },
+    legend:{},
+    grid:{
+      left:'3%',
+      right: '4%',
+      bottom: '3%',
+      containLabel: true,
+      tooltip:{
+        formatter: function(params: { value: any; }){
+          console.log(params.value);
+        }
+      }
+    },
+    xAxis:{
+      type: 'value',
+      boundaryGap: [0,0.01]
+    },
+    yAxis:{
+      type: 'category',
+      data:['José','Paulo','Vieira','Constantino','Makoto & Makoto']
+    },
+    series:[{
+      name: 'Última Coleção',
+      type: 'bar',
+      data:[10000, 25000, 15000, 12000, 18000]
+    }]
+  }
+
+  constructor(){
+
+  }
+
+  ngOnInit(): void {
+    
+  }
 
 }
