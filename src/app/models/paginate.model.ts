c16599d7ab8f7ca8fc8dcba503b4e2e80b334b06
evertonrivas <evertonrivas@gmail.com>
2023-05-03 11:15:54 -0300

    Novos modelos de dados para o sistema

---
+++ src/app/models/paginate.model.ts
@@ -0,0 +1,7 @@
+export interface Paginate{
+    registers: number,
+    page:number,
+    per_page:number,
+    pages:number,
+    has_next:boolean
+}
---
+++ src/app/models/user.model.ts
@@ -0,0 +1,23 @@
+import { Paginate } from "./paginate.model"
+
+export interface User {
+    username:string,
+    password: string | null,
+    type:string,
+    date_created: string,
+    date_updated: string,
+    active: boolean
+}
+
+export interface UserOptions{
+    orderBy:string | null,
+    orderDir:'ASC' | 'DESC',
+    pagSize:number | null,
+    search:string | null,
+    page:number
+}
+
+export interface UserResponse{
+    pagination: Paginate
+    data: User[]
+}
