1a76c60b6aed2e80b181f94e7c0d80f6a2a35916
evertonrivas <evertonrivas@gmail.com>
2023-05-03 11:15:26 -0300

    Classe padrão para paginação

--- src/app/admin/pagination.ts
+++ src/app/admin/pagination.ts
@@ -1,22 +1,43 @@
+import { Paginate } from "../models/paginate.model";
+
+interface Response{
+    pagination: Paginate,
+    data: any
+}
+
 export class Pagination {
-    response:any;
+    response:Response;
     options:any;
+
+    constructor(){
+        this.response = {
+            pagination : {
+                page : 0,
+                has_next : false,
+                pages : 0,
+                per_page : 0,
+                registers: 0
+            },
+            data : undefined
+        }
+    }
+
     get numbers(): number[]{
         //funcao necessaria para formatar as pagianas da paginacao
-        const limit = this.response.paginate.pages;
+        const limit = this.response.pagination.pages;
         let retorno = Array.from({length: limit}, (_,i) => i+1);
         return retorno;
     }
 
     next(){
-
+        this.options.page++;
     }
 
     prev(){
-
+        this.options.page--;
     }
 
     to(page:number){
-
+        this.options.page = page;
     }
 }
