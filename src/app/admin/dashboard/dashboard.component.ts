2af915c1204bffbce1b46074228c3586c4d1c314
evertonrivas <evertonrivas@gmail.com>
2023-05-11 21:15:53 -0300

    Correções no dashboard

--- src/app/admin/dashboard/dashboard.component.ts
+++ src/app/admin/dashboard/dashboard.component.ts
@@ -1,6 +1,8 @@
 import { formatCurrency } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { EChartsOption} from 'echarts';
+import { EntityType } from 'src/app/models/entity.model';
+import { DashboardService } from 'src/app/services/dashboard.service';
 
 
 @Component({
@@ -13,7 +15,7 @@ export class DashboardComponent  implements OnInit{
   totalOrder:number = 0;
   totalRepresentative:number = 0;
   totalCustomer:number = 0;
-  options:EChartsOption = {
+  chartRepresentative:EChartsOption = {
     tooltip:{
       trigger: 'axis',
       axisPointer:{
@@ -44,16 +46,63 @@ export class DashboardComponent  implements OnInit{
     series:[{
       name: 'Última Coleção',
       type: 'bar',
-      data:[10000, 25000, 15000, 12000, 18000]
+      data:[10000, 25000, 15000, 12000, 18000],
+      markLine:{
+        data:[{ type: 'average', name: 'Média'}]
+      }
     }]
   }
 
-  constructor(){
+  constructor(private svc:DashboardService){
 
   }
 
   ngOnInit(): void {
-    
+    this.svc.countEntity(EntityType.C).subscribe((data) =>{ this.totalCustomer = data; });
+    this.svc.countEntity(EntityType.R).subscribe((data) =>{ this.totalRepresentative = data; });
+    /*this.svc.countOrder().subscribe((data) => { this.totalOrder = data; });
+    this.svc.valueOrder().subscribe((data) => { this.totalOrder = data; });
+    this.svc.valueOrderByRepresentative().subscribe({
+      next: response =>{
+        this.chartRepresentative = {
+          tooltip:{
+            trigger: 'axis',
+            axisPointer:{
+              type: 'shadow'
+            },
+            valueFormatter: (value) => formatCurrency(parseFloat(value.toString()),"pt","R$")
+          },
+          legend:{},
+          grid:{
+            left:'3%',
+            right: '4%',
+            bottom: '3%',
+            containLabel: true,
+            tooltip:{
+              formatter: function(params: { value: any; }){
+                console.log(params.value);
+              }
+            }
+          },
+          xAxis:{
+            type: 'value',
+            boundaryGap: [0,0.01]
+          },
+          yAxis:{
+            type: 'category',
+            data: response.representatives
+          },
+          series:[{
+            name: 'Última Coleção',
+            type: 'bar',
+            data: response.values,
+            markLine:{
+              data:[{ type: 'average', name: 'Média'}]
+            }
+          }]
+        }
+      }
+    });*/
   }
 
 }
