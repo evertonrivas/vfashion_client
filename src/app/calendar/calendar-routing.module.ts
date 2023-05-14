b44ef29b072106a077eb686b5491c027862beb6d
evertonrivas <evertonrivas@gmail.com>
2023-05-14 11:01:34 -0300

    Inclusa rota do component chart

--- src/app/calendar/calendar-routing.module.ts
+++ src/app/calendar/calendar-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CalendarComponent } from './calendar.component';
+import { ChartComponent } from './chart/chart.component';
 
-const routes: Routes = [{ path: '', component: CalendarComponent }];
+const routes: Routes = [{ 
+  path: '', 
+  component: CalendarComponent,
+  children:[{
+    path: 'chart',
+    component: ChartComponent
+  }]
+}];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
