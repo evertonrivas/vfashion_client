6a467a793be05471f88c80519e1d81be3d3318c0
evertonrivas <evertonrivas@gmail.com>
2023-05-14 11:01:44 -0300

    Versão inicial

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { SecurityService } from '../services/security.service';
+import { Subscription } from 'rxjs';
+import * as configData from 'src/assets/config.json';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-calendar',
   templateUrl: './calendar.component.html',
   styleUrls: ['./calendar.component.scss']
 })
-export class CalendarComponent {
+export class CalendarComponent implements OnInit{
+  authSub:Subscription = new Subscription;
+  company_logo:string = '';
+  sys_config:any = (configData as any).default;
 
+  constructor(private auth:SecurityService, 
+    private myRoute:Router){
+    this.company_logo = this.sys_config.company.logo_mini;
+  }
+  ngOnInit(): void {
+    this.myRoute.navigate(["/calendar/chart"]);
+  }
+
+  logout():void{
+    this.authSub = this.auth.logoff().subscribe(()=>{
+      document.location.href='/';
+    });
+  }
+
+  onSubmit():void{
+
+  }
 }
