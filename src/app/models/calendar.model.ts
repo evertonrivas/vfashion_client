6941fee590715927368fc4a44fbd3ad57a1603f3
evertonrivas <evertonrivas@gmail.com>
2023-05-14 23:01:56 -0300

    Interface de trabalho do calendário

---
+++ src/app/models/calendar.model.ts
@@ -0,0 +1,25 @@
+export interface Calendar{
+    year: number,
+    month: number,
+    week: number
+}
+
+export interface CalendarOptions{
+    search:string
+}
+
+export interface EventTypeOptions{
+    orderBy:string | null,
+    orderDir:'ASC' | 'DESC',
+    pagSize:number | null,
+    search:string | null,
+    page:number
+    list_all: boolean
+}
+
+export interface CalendarEventType{
+    id: number,
+    name:string,
+    hex_color:string,
+    has_budget:boolean
+}
