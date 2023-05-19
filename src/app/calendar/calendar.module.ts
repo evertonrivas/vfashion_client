1fe40b0dd2a862ed067006fc7ce66b8796cc5e48
evertonrivas <evertonrivas@gmail.com>
2023-05-19 08:50:21 -0300

    Novos componentes e módulos

--- src/app/calendar/calendar.module.ts
+++ src/app/calendar/calendar.module.ts
@@ -4,16 +4,31 @@ import { CommonModule } from '@angular/common';
 import { CalendarRoutingModule } from './calendar-routing.module';
 import { CalendarComponent } from './calendar.component';
 import { ChartComponent } from './chart/chart.component';
+import { EventComponent } from './event/event.component';
+import { AngularSplitModule } from 'angular-split';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgxMaskPipe, NgxMaskDirective, provideNgxMask } from 'ngx-mask';
+import { NgxColorsModule } from 'ngx-colors';
 
 
 @NgModule({
   declarations: [
     CalendarComponent,
-    ChartComponent
+    ChartComponent,
+    EventComponent
   ],
   imports: [
     CommonModule,
-    CalendarRoutingModule
+    CalendarRoutingModule,
+    AngularSplitModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NgxMaskPipe, 
+    NgxMaskDirective,
+    NgxColorsModule
+  ],
+  providers:[
+    provideNgxMask()
   ]
 })
 export class CalendarModule { }
