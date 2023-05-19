2f02f15fd3f3b24c91fb7a02d9d27aa70ac0b840
evertonrivas <evertonrivas@gmail.com>
2023-05-19 08:48:37 -0300

    alterado de lugar

--- src/app/admin/datamanipulation.spec.ts
+++
@@ -1,7 +0,0 @@
-import { DataManipulation } from './datamanipulation';
-
-describe('DataManipulation', () => {
-  it('should create an instance', () => {
-    expect(new DataManipulation()).toBeTruthy();
-  });
-});
