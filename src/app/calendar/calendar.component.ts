a93b8e0ffd6801b0ebc4fe971797da4769ec92a1
evertonrivas <evertonrivas@gmail.com>
2023-05-19 08:50:47 -0300

    Componente para gestão do calendário

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -13,13 +13,35 @@ export class CalendarComponent implements OnInit{
   authSub:Subscription = new Subscription;
   company_logo:string = '';
   sys_config:any = (configData as any).default;
+  idTimer = 0;
 
   constructor(private auth:SecurityService, 
     private myRoute:Router){
     this.company_logo = this.sys_config.company.logo_mini;
   }
   ngOnInit(): void {
+    this.checkLogged();
+    this.idTimer = <any>setInterval(() =>{
+      this.checkLogged();
+    },30000);//verifica a cada 30 segundos
     this.myRoute.navigate(["/calendar/chart"]);
+  }
+
+  checkLogged():void{
+    this.authSub = this.auth.checkLogged().subscribe({
+      next: data => {
+        if (data==false){
+          document.location.href="/";
+        }else{
+          let dt_token = Date.parse( String(localStorage.getItem('token_expire')) );
+          let now = Date.now();
+          const diffTime = Math.abs(dt_token - now);
+          if(diffTime<=60000){
+            //colocar aqui o alerta de que o tempo esta acabando
+          }
+        }
+      }
+    });
   }
 
   logout():void{
@@ -28,7 +50,4 @@ export class CalendarComponent implements OnInit{
     });
   }
 
-  onSubmit():void{
-
-  }
 }
