f684a8b3250b6a6d5d628668abf1ad81decbc9a3
evertonrivas <evertonrivas@gmail.com>
2023-05-19 23:28:59 -0300

    Objeto de milestones para gantt

---
+++ src/app/calendar/milestone/milestone.component.ts
@@ -0,0 +1,12 @@
+import { Component,Input } from '@angular/core';
+
+@Component({
+  selector: 'app-milestone',
+  templateUrl: './milestone.component.html',
+  styleUrls: ['./milestone.component.scss']
+})
+export class MilestoneComponent {
+  @Input() ComponentColor:string = "";
+  @Input() ComponentSize:number = 0;
+  @Input() EventName:string = "";
+}
