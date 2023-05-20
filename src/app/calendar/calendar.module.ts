eab44a4da6f696cdc214aaff075460cfd8364ce7
evertonrivas <evertonrivas@gmail.com>
2023-05-19 23:30:47 -0300

    Troca de Chart para Gantt e inclusão de milestone

--- src/app/calendar/calendar.module.ts
+++ src/app/calendar/calendar.module.ts
@@ -3,19 +3,21 @@ import { CommonModule } from '@angular/common';
 
 import { CalendarRoutingModule } from './calendar-routing.module';
 import { CalendarComponent } from './calendar.component';
-import { ChartComponent } from './chart/chart.component';
+import { GanttComponent } from './gantt/gantt.component';
 import { EventComponent } from './event/event.component';
 import { AngularSplitModule } from 'angular-split';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgxMaskPipe, NgxMaskDirective, provideNgxMask } from 'ngx-mask';
 import { NgxColorsModule } from 'ngx-colors';
+import { MilestoneComponent } from './milestone/milestone.component';
 
 
 @NgModule({
   declarations: [
     CalendarComponent,
-    ChartComponent,
-    EventComponent
+    GanttComponent,
+    EventComponent,
+    MilestoneComponent
   ],
   imports: [
     CommonModule,
