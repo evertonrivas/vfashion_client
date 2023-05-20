7640c02d2f997d261b52a2fec5284b88b5bb93a2
evertonrivas <evertonrivas@gmail.com>
2023-05-19 23:31:30 -0300

    Ajustes em dados do calendário

--- src/app/services/calendar.service.ts
+++ src/app/services/calendar.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
-import { MyHttp } from './my-http';
+import { ContentType, MyHttp } from './my-http';
 import { HttpClient, HttpParams } from '@angular/common/http';
-import { Calendar, CalendarEventType, CalendarOptions, EventTypeOptions } from '../models/calendar.model';
+import { Calendar, CalendarEvent, CalendarEventData, CalendarEventType, CalendarOptions, EventTypeOptions } from '../models/calendar.model';
 import { Observable } from 'rxjs';
 
 @Injectable({
@@ -20,23 +20,35 @@ export class CalendarService extends MyHttp{
     });
   }
 
+  calendarEventLoad(options:CalendarOptions):Observable<CalendarEvent[]>{
+    return this.http.get<CalendarEvent[]>(this.sys_config.backend_scm+'/calendar/events',{
+      headers: this.getHeader(),
+      params: new HttpParams().set("query",options.search).set("milestone",options.milestone)
+    })
+  }
+
+  calendarEventSave(evt:CalendarEventData):Observable<any>{
+    let data = {
+        name: evt.name,
+        date_start: evt.date_start,
+        date_end: evt.date_end,
+        id_event_type: evt.id_event_type,
+        id_collection: evt.id_collection,
+        budget_value: evt.budget_value,
+      }
+    return this.http.post<any>(this.sys_config.backend_scm+'/calendar/'+(evt.id!=0?evt.id.toString():''),data,{
+      headers: this.getHeaderPost(ContentType.json)
+    });
+  }
+
   eventTypeSave(event:CalendarEventType):Observable<boolean>{
-    let data;
-    if (event.id!=0){
-      data = {
-        id: event.id,
-        name: event.name,
-        hex_color: event.hex_color,
-        has_budget: event.has_budget
-      }
-    }else{
-      data = {
-        name: event.name,
-        hex_color: event.hex_color,
-        has_budget: event.has_budget
-      }
+    let data = {
+      name: event.name,
+      hex_color: event.hex_color,
+      has_budget: event.has_budget
     }
-    return this.http.post<boolean>(this.sys_config.backend_scm+'/event-type',data,{
+
+    return this.http.post<boolean>(this.sys_config.backend_scm+'/event-type/'+(event.id!=0?event.id.toString():''),data,{
       headers: this.getHeader(true)
     });
   }
@@ -55,7 +67,7 @@ export class CalendarService extends MyHttp{
       myParams = myParams.set("order_dir",options.orderDir);
     }
     myParams = myParams.set("list_all",options.list_all);
-    return this.http.get<CalendarEventType[]>(this.sys_config.backend_scm+'/event-type',{
+    return this.http.get<CalendarEventType[]>(this.sys_config.backend_scm+'/event-type/',{
       headers: this.getHeader(),
       params: myParams
     });
