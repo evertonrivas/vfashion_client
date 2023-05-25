c5b13194a29bf62bddea5adabfb5debfa59548b0
evertonrivas <evertonrivas@gmail.com>
2023-05-25 15:55:18 -0300

    Uso do Dashboard no calendário

--- src/app/calendar/calendar-routing.module.ts
+++ src/app/calendar/calendar-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CalendarComponent } from './calendar.component';
 import { GanttComponent } from './gantt/gantt.component';
+import { DashboardComponent } from '../dashboard/dashboard.component';
 
 const routes: Routes = [{ 
   path: '', 
@@ -9,6 +10,9 @@ const routes: Routes = [{
   children:[{
     path: 'chart',
     component: GanttComponent
+  },{
+    path: 'dashboard',
+    component: DashboardComponent
   }]
 }];
 
