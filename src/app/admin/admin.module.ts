7782e0418bc333ef079879d0f1ca47501abbd9fc
evertonrivas <evertonrivas@gmail.com>
2023-05-25 15:55:05 -0300

    Reestruturação com SharedModule

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,10 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule,DATE_PIPE_DEFAULT_OPTIONS, registerLocaleData } from '@angular/common';
 
-import { NgxEchartsModule } from 'ngx-echarts';
 import { AdminRoutingModule } from './admin-routing.module';
 import { AdminComponent } from './admin.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { LOCALE_ID, DEFAULT_CURRENCY_CODE } from '@angular/core';
 import ptBr from '@angular/common/locales/pt';
@@ -12,62 +10,43 @@ import * as sys_config from 'src/assets/config.json';
 import { UsersComponent } from './users/users.component';
 import { EntitiesComponent } from './entities/entities.component';
 import { ConfigComponent } from './config/config.component';
-import { CnpjCpfPipe } from '../pipes/cnpj-cpf.pipe';
-import { EntityTypePipe } from '../pipes/entity-type.pipe';
-import { UserTypePipe } from '../pipes/user-type.pipe';
-import { NameCutPipe } from '../pipes/name-cut.pipe';
-import { ShortMoneyPipe } from '../pipes/short-money.pipe';
 import { ProfileComponent } from './profile/profile.component';
 import { NgxColorsModule } from 'ngx-colors';
+import { SharedModule } from '../shared/shared.module';
 
 registerLocaleData(ptBr)
 
 @NgModule({
-  declarations: [
-    AdminComponent,
-    DashboardComponent,
-    UsersComponent,
-    EntitiesComponent,
-    ConfigComponent,
-    CnpjCpfPipe,
-    EntityTypePipe,
-    UserTypePipe,
-    NameCutPipe,
-    ShortMoneyPipe,
-    ProfileComponent
-  ],
-  imports: [
-    CommonModule,
-    AdminRoutingModule,
-    FormsModule,
-    ReactiveFormsModule,
-    NgxColorsModule,
-    NgxEchartsModule.forRoot({
-      echarts: () => import('echarts')
-    })
-  ],
-  exports:[
-    CnpjCpfPipe,
-    EntityTypePipe,
-    UserTypePipe,
-    NameCutPipe,
-    ShortMoneyPipe
-  ],
-  providers:[
-    {
-      provide: LOCALE_ID,
-      useValue: ((sys_config as any).default).locale.language
-    },
-    {
-      provide: DEFAULT_CURRENCY_CODE,
-      useValue: ((sys_config as any).default).locale.currency_code
-    },{
-      provide: DATE_PIPE_DEFAULT_OPTIONS,
-      useValue: {
-        dateFormat: ((sys_config as any).default).locale.date_format,
-        timezone: ((sys_config as any).default).locale.timezone
-      }
-    }
-  ]
+    declarations: [
+        AdminComponent,
+        UsersComponent,
+        EntitiesComponent,
+        ConfigComponent,
+        ProfileComponent
+    ],
+    providers: [
+        {
+            provide: LOCALE_ID,
+            useValue: ((sys_config as any).default).locale.language
+        },
+        {
+            provide: DEFAULT_CURRENCY_CODE,
+            useValue: ((sys_config as any).default).locale.currency_code
+        }, {
+            provide: DATE_PIPE_DEFAULT_OPTIONS,
+            useValue: {
+                dateFormat: ((sys_config as any).default).locale.date_format,
+                timezone: ((sys_config as any).default).locale.timezone
+            }
+        }
+    ],
+    imports: [
+        CommonModule,
+        AdminRoutingModule,
+        FormsModule,
+        ReactiveFormsModule,
+        NgxColorsModule,
+        SharedModule
+    ]
 })
 export class AdminModule { }
