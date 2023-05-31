b4d89307f585eb21cc7736ace0ccc62eb692d5be
evertonrivas <evertonrivas@gmail.com>
2023-05-31 12:54:06 -0300

    Add Text to Card

--- src/app/dashboard/card/card.component.ts
+++ src/app/dashboard/card/card.component.ts
@@ -6,12 +6,14 @@ import { Component,Input } from '@angular/core';
   styleUrls: ['./card.component.scss']
 })
 export class CardComponent {
-  @Input() isMoney:boolean   = false;
-  @Input() isNumber:boolean  = false;
-  @Input() value:number      = 0;
-  @Input() icon:string|null  = null;
-  @Input() title:string|null = null;
-  @Input() subtitle:string|null=null;
+  @Input() isMoney:boolean      = false;
+  @Input() isNumber:boolean     = false;
+  @Input() isText:boolean       = false;
+  @Input() icon:string|null     = null;
+  @Input() title:string|null    = null;
+  @Input() subtitle:string|null = null;
+  @Input() text:string|null     = null;
+  @Input() value:number         = 0;
 
   constructor(){
 
