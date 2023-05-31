167798dc0c9c7b716f759c8ed9cba3154387dde3
evertonrivas <evertonrivas@gmail.com>
2023-05-31 12:00:56 -0300

    Cards do Dashboard

--- src/app/dashboard/card/card.component.ts
+++ src/app/dashboard/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,Input } from '@angular/core';
 
 @Component({
   selector: 'app-card',
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./card.component.scss']
 })
 export class CardComponent {
+  @Input() isMoney:boolean   = false;
+  @Input() isNumber:boolean  = false;
+  @Input() value:number      = 0;
+  @Input() icon:string|null  = null;
+  @Input() title:string|null = null;
+  @Input() subtitle:string|null=null;
 
+  constructor(){
+
+  }
 }
